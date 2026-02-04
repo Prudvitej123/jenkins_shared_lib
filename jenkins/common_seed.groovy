5311fb2ebcff764b1a803b5aee0297b8649c622e
Prudvitej123 <prudvigowd2002@gmail.com>
2026-02-04 16:16:15 +0530

    Update common_seed.groovy

--- jenkins/common_seed.groovy
+++ jenkins/common_seed.groovy
@@ -1,33 +1,12 @@
-/***********************
- * GLOBAL VARIABLES
- ***********************/
 def baseUrl = "https://github.com/Prudvitej123/"
-def repoName = reponame                      // Jenkins parameter
+def repoName = reponame
 def gitRepoUrl = baseUrl + repoName + ".git"
 
 def jobName = reponame
-def branchSpec = branch ?: "*/main,*/master"
+def branchSpec = "*/main,*/master"   // 🔥 FIXED
 def jenkinsfilePath = "Jenkinsfile"
 
-/***********************
- * PIPELINE JOB
- ***********************/
 pipelineJob(jobName) {
-
-    properties {
-        pipelineTriggers {
-            triggers {
-                pollSCM {
-                    scmpoll_spec('*/1 * * * *')
-                    ignorePostCommitHooks(true)
-                }
-            }
-        }
-    }
-
-    logRotator {
-        numToKeep(5)
-    }
 
     definition {
         cpsScm {
@@ -37,13 +16,11 @@ pipelineJob(jobName) {
                         url(gitRepoUrl)
                         credentials('github_credentials')
                     }
-                    branches(branchSpec)   // ✅ FIXED HERE
-                    extensions {
-                        cleanBeforeCheckout()
-                    }
+                    branches(branchSpec)
                 }
             }
             scriptPath(jenkinsfilePath)
         }
     }
 }
+
