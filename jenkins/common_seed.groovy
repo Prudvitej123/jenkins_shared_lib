136f8cf8625bc9a79d6511998dcb899c818c0658
Prudvitej123 <prudvigowd2002@gmail.com>
2026-02-04 12:23:42 +0530

    Update common_seed.groovy

--- jenkins/common_seed.groovy
+++ jenkins/common_seed.groovy
@@ -1,4 +1,4 @@
-def baseUrl = "git@github.com:Prudvitej123/jenkins_shared_lib.git"
+def baseUrl = "https://github.com/Prudvitej123/jenkins_shared_lib"
 def repoName = "$jobname"
 def gitRepoUrl = baseUrl + repoName + ".git"
 def jobName = "$reponame"
